43b0ff40c791674f19638efacc6020e7b18ecf33
13919201819 <humaan01012001@gmail.com>
2026-03-01 14:39:46 +0530

    Change base path in Vite config to root

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/145211815615187519-New/'
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/145211815615187519-New/'
+  base: '/'
 })
